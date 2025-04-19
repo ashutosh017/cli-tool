8f977bb271b551c25d0bf5ba3028e6203f792b5a
ashutosh017 <wwwashutosh225@gmail.com>
2025-04-19 20:23:01 +0530

    remove unwanted comments and fix some bugs

--- tool/src/commands/start.js
+++ tool/src/commands/start.js
@@ -1,11 +1,7 @@
-import chalk from 'chalk'
 import createLogger from '../logger.js'
 const logger = createLogger("commands:start")
 function start(config){
-    // console.log(chalk.bgCyanBright("starting the app"))
     logger.highlight("starting the app")
-    // console.log("logging config in tool/src/commands/start.js: ",config)
-    // console.log(chalk.gray("recieved configuration in start: "),config)
     logger.debug("recieved configuration is start: ",config)
 }
 
